f20ff334f0e6023bf0b289233278ac4112b8735f
p.puczka <p.puczka@gmail.com>
2020-02-27 22:41:03 +0100

    create file command working, fliget render text

--- openSSLConf.py
+++ openSSLConf.py
@@ -1,43 +1,93 @@
 import argparse
 import configparser
 import sys
+from pyfiglet import Figlet
+class ConfigFile:
 
-class FileParameters:
+    # countryName = ""
+    # stateOrProvinceName = ""
+    # localityName = ""
+    # organizationName = ""
+    # organizationalUnitName = ""
+    # emailAddress = ""
+    # commonName = ""
+    # dns = ""
+    # ip = ""
 
-    def __init__(self, ) 
+# Describe the Subject (ie the origanisation).
+# The first 6 below could be shortened to: C ST L O OU CN
+# The short names are what are shown when the certificate is displayed.
+# Eg the details below would be shown as:
+#    Subject: C=UK, ST=Hertfordshire, L=My Town, O=Some Organisation, OU=Some Department, CN=www.example.com/emailAddress=bofh@example.com
 
-def create_config_file():
-    file_creator = configparser.ConfigParser()
-    file_creator.add_section('crt')
-    file_creator.set('crt', 'default_bits', '2048')
-    file_creator.set('crt', 'prompt', 'no')
-    file_creator.set('crt', 'default_md', 'sha256')
-    file_creator.set('crt', 'req_extensions', 'req_ext')
-    file_creator.set('crt', 'distinguished_name', 'dn')
-    file_creator.add_section('dn')
-    file_creator.set('dn', 'C', )
-    file_creator.set('dn', 'ST', )
-    file_creator.set('dn', 'L', )
-    file_creator.set('dn', 'O', )
-    file_creator.set('dn', 'OU', )
-    file_creator.set('dn', 'emailAddress', 'replace with email')
-    file_creator.set('dn', 'CN', )
-    file_creator.add_section('req_ext')
-    file_creator.set('req_ext', 'subjectAltName', '@alt_names')
-    file_creator.add_section('alt_names')
-    file_creator.set('alt_names', 'DNS.1', 'replace_this')
-    file_creator.set('alt_names', 'DNS.2', 'replace_this')
-    file_creator.set('alt_names', 'IP.1', 'replace_this')
-    file_creator.write(sys.stdout)
+    def __init__(self, 
+                countryName = "", 
+                stateOrProvinceName = "",
+                localityName = "",
+                organizationName = "",
+                organizationalUnitName = "",
+                commonName = "",
+                emailAddress = "",
+                dns = "",
+                ip = ""):
+        
+        self.countryName = input(f"provide value for countryName ")
+        self.stateOrProvinceName = input(f"provide value for stateOrProvinceName ")
+        self.localityName = input(f"provide value for localityName ")
+        self.organizationName = input(f"provide value for organizationName ")
+        self.organizationalUnitName = input(f"provide value for organizationalUnitName ")
+        self.emailAddress = input(f"provide value for emailAddress " )
+        self.commonName = input(f"provide value for commonName ")
+        self.dns = input(f"provide value for dns ")
+        self.ip = input(f"provide value for ip ")
+        
+    def create_config_file(self):
+        file_name = input("Provide filename: ")
 
-    with open("file2.conf", "w") as conf:
-        file_creator.write(conf)
+        file_creator = configparser.ConfigParser()
+        file_creator.add_section('crt')
+        file_creator.set('crt', 'default_bits', '2048')
+        file_creator.set('crt', 'prompt', 'no')
+        file_creator.set('crt', 'default_md', 'sha256')
+        file_creator.set('crt', 'req_extensions', 'req_ext')
+        file_creator.set('crt', 'distinguished_name', 'dn')
+        file_creator.add_section('dn')
+        file_creator.set('dn', 'C', self.countryName)
+        file_creator.set('dn', 'ST', self.stateOrProvinceName)
+        file_creator.set('dn', 'L', self.stateOrProvinceName)
+        file_creator.set('dn', 'O', self.organizationalUnitName)
+        file_creator.set('dn', 'OU', self.organizationalUnitName)
+        file_creator.set('dn', 'emailAddress', self.emailAddress)
+        file_creator.set('dn', 'CN', self.commonName)
+        file_creator.add_section('req_ext')
+        file_creator.set('req_ext', 'subjectAltName', '@alt_names')
+        file_creator.add_section('alt_names')
+        file_creator.set('alt_names', 'DNS.1', self.dns)
+        file_creator.set('alt_names', 'IP.1', self.ip)
+        file_creator.write(sys.stdout)
 
+        with open(f"{file_name}.conf", "w") as conf:
+            file_creator.write(conf)
 
-parser = argparse.ArgumentParser(description='Create openSSL config files with one command')
-parser.add_argument('-v', '--version', action = 'version', version = '1.0.0')
-parser.add_argument('-c', '--create', dest = 'create_file', action = 'store_true', help = "creates openSSL config file")
-args = parser.parse_args()
+    def __str__(self):
+        return  'Entered values:\n' + ', '.join(['{key} = {value}'.format(key=key, value=self.__dict__.get(key)) for key in self.__dict__]) 
 
-if args.create_file:
+def main():
 
+    parser = argparse.ArgumentParser(description='Create openSSL config files with one command')
+    parser.add_argument('-v', '--version', action = 'version', version = '1.0.0')
+    parser.add_argument('-c', '--create', dest = 'create_file', action = 'store_true', help = "creates openSSL config file")
+    args = parser.parse_args()
+
+    if args.create_file:
+        fliget = Figlet(font='slant')
+        print(fliget.renderText('OpenSSL Config'))
+        user_input = ""
+        while (user_input.capitalize() != "Y"):
+            new_file = ConfigFile()
+            print(new_file)
+            user_input = input("Is this correct Y/N ? ")
+        new_file.create_config_file()
+
+if __name__=="__main__":
+    main()
